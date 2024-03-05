45d05261aa75d113590eca4dfd39d610cee99d8f
Oleg <shlyapin8848@gmail.com>
2024-03-05 14:55:00 +0200

    add some filters for handlers

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import asyncio
 import os
+
+from aiogram.types import BotCommandScopeAllPrivateChats
 from dotenv import find_dotenv, load_dotenv
 from aiogram import Dispatcher, Bot
+
 load_dotenv(find_dotenv())
 from handlers.user_private import user_private_router                 # Импортируем роутеры для общения с пользовательскими сообщениями
+from commands.bot_cmds_list import private
 
 
 ALLOW_UPDATES = ["message", "edited_message"]
+
+
 
 
 bot = Bot(token=os.getenv("TOKEN"))
@@ -16,6 +22,7 @@ dp.include_router(user_private_router)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)                   # Удалять все входящие сообщения, пока бот не в сети
+    await bot.set_my_commands(commands=private, scope=BotCommandScopeAllPrivateChats())  # Добавляем к боту команды, а также выставляем зону их отображения
     await dp.start_polling(bot, allowed_updates=ALLOW_UPDATES)     # Указываем какие обновления будет обрабатывать наш ТГ бот
 
 
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,5 +1,5 @@
-from aiogram.filters import CommandStart, Command
-from aiogram import types, Router
+from aiogram.filters import CommandStart, Command, or_f
+from aiogram import types, Router, F
 
 user_private_router = Router()
 
@@ -9,6 +9,20 @@ async def start(message: types.Message):
     await message.answer("Добро пожаловать в нашу пиццерию, рады Вас видеть!!!")
 
 
-@user_private_router.message(Command("menu"))
+@user_private_router.message(or_f(Command("menu"), F.text.lower().contains('меню')))
 async def menu_command(message: types.Message):
     await message.answer("Вот наше меню:")
+
+@user_private_router.message(Command("about"))
+async def menu_command(message: types.Message):
+    await message.answer("O нас - ")
+
+@user_private_router.message(Command("payment"))
+async def menu_command(message: types.Message):
+    await message.answer("Варианты оплаты - ")
+
+@user_private_router.message((F.text.lower().contains("доставк")) | (F.text.lower() == "варианты доставки"))
+@user_private_router.message(Command("shipping"))
+async def menu_command(message: types.Message):
+    await message.answer("Варианты доставки - ")
+
